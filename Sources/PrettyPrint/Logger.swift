735332ed5a4710dce039fc965d4dd6aff3ee62d6
Martin Pfundmair <dev@martin.pfundmair.de>
2021-01-03 12:03:10 +0900

    initial Commit

---
+++ Sources/PrettyPrint/Logger.swift
@@ -0,0 +1,41 @@
+//
+//  LogService.swift
+//
+//  Created by Martin Pfundmair on 2018-05-20.
+//  Copyright © 2018 Martin Pfundmair. All rights reserved.
+//
+
+import Foundation
+
+protocol Logger {
+    static func setup() -> Logger
+    func verbose(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int)
+    func debug(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int)
+    func info(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int)
+    func warning(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int)
+    func error(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int)
+    func severe(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int)
+}
+
+extension Logger {
+
+    func verbose(_ closure: @autoclosure () -> Any?, _ path: StaticString? = #file, _ function: StaticString = #function, line: Int = #line) {
+        verbose(closure(), path!, function, line: line)
+    }
+
+    func debug(_ closure: @autoclosure () -> Any?, _ path: StaticString? = #file, _ function: StaticString = #function, line: Int = #line) {
+        debug(closure(), path!, function, line: line)
+    }
+    func info(_ closure: @autoclosure () -> Any?, _ path: StaticString? = #file, _ function: StaticString = #function, line: Int = #line) {
+        info(closure(), path!, function, line: line)
+    }
+    func warning(_ closure: @autoclosure () -> Any?, _ path: StaticString? = #file, _ function: StaticString = #function, line: Int = #line) {
+        warning(closure(), path!, function, line: line)
+    }
+    func error(_ closure: @autoclosure () -> Any?, _ path: StaticString? = #file, _ function: StaticString = #function, line: Int = #line) {
+        error(closure(), path!, function, line: line)
+    }
+    func severe(_ closure: @autoclosure () -> Any?, _ path: StaticString? = #file, _ function: StaticString = #function, line: Int = #line) {
+        severe(closure(), path!, function, line: line)
+    }
+}
---
+++ Sources/PrettyPrint/PrintLogger.swift
@@ -0,0 +1,65 @@
+//
+//  PrintLogger.swift
+//
+//  Created by Martin Pfundmair on 2018-05-20.
+//  Copyright © 2018 Martin Pfundmair. All rights reserved.
+//
+
+import Foundation
+
+class PrintLogger: Logger {
+
+    static var dateFormatter: DateFormatter = {
+        let defaultDateFormatter = DateFormatter()
+        defaultDateFormatter.locale = NSLocale.current
+        defaultDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        return defaultDateFormatter
+    }()
+
+    var dateString: String { PrintLogger.dateFormatter.string(from: Date()) }
+
+    static func setup() -> Logger { PrintLogger() }
+
+    func verbose(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int) {
+        guard let closure = closure() else { return }
+        #if DEBUG
+        print("\(dateString) ⚪️ [\(String(path).split(separator: "/").last ?? ""):\(line)] \(String(describing: closure))")
+        #endif
+    }
+
+    func debug(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int) {
+        guard let closure = closure() else { return }
+        #if DEBUG
+        print("\(dateString) 🔶 [\(String(path).split(separator: "/").last ?? ""):\(line)] \(String(describing: closure))")
+        #endif
+    }
+
+    func info(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int) {
+        guard let closure = closure() else { return }
+        #if DEBUG
+        print("\(dateString) 💚 [\(String(path).split(separator: "/").last ?? ""):\(line)] \(String(describing: closure))")
+        #endif
+    }
+
+    func warning(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int) {
+        guard let closure = closure() else { return }
+        #if DEBUG
+        print("\(dateString) ⚠️ [\(String(path).split(separator: "/").last ?? ""):\(line)] \(String(describing: closure))")
+        #endif
+    }
+
+    func error(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int) {
+        guard let closure = closure() else { return }
+        #if DEBUG
+        print("\(dateString) 💔 [\(String(path).split(separator: "/").last ?? ""):\(line)] \(String(describing: closure))")
+        #endif
+    }
+
+    func severe(_ closure: @autoclosure () -> Any?, _ path: StaticString, _ function: StaticString, line: Int) {
+        guard let closure = closure() else { return }
+        #if DEBUG
+        print("\(dateString) ☠️ [\(String(path).split(separator: "/").last ?? ""):\(line)] \(String(describing: closure))")
+        #endif
+    }
+
+}
